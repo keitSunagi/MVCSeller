2d0a1ca8d0223107cf9f47529358c9e843e99898
Lucas Lopes <lukas-lopes15@outlook.com>
2023-05-19 10:43:41 -0300

    CRUD Scaffolding

--- SallesWebMVC/SallesWebMVC/Program.cs
+++ SallesWebMVC/SallesWebMVC/Program.cs
@@ -1,3 +1,6 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SallesWebMVC.Data;
 namespace SallesWebMVC
 {
     public class Program
@@ -5,6 +8,8 @@ namespace SallesWebMVC
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<SallesWebMVCContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("SallesWebMVCContext") ?? throw new InvalidOperationException("Connection string 'SallesWebMVCContext' not found.")));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
