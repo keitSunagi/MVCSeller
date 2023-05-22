9684a44a802f75342f6c99bc759e2c70222aa438
Lucas Lopes <lukas-lopes15@outlook.com>
2023-05-22 09:12:32 -0300

    Crud Scafolding da classe Departments e primeira Migration da DataBase

---
+++ WebSalesMVC/WebSalesMVC/Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebSalesMVC.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WebSalesMVC/WebSalesMVC/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebSalesMVC.Data;
+namespace WebSalesMVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var serverVersion = new MySqlServerVersion(new Version(8,0,33));
+
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<WebSalesMVCContext>(options =>
+                options.UseMySql(builder.Configuration.GetConnectionString("WebSalesMVCContext"), serverVersion,builder => builder.MigrationsAssembly("WebSalesMVC")));
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
